a2828ba74197066de3fea20194ebfb013813710f
Roman <bardlesswk@gmail.com>
2019-12-25 21:09:18 +0200

    Practice

---
+++ Practice13/Gym.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice13
+{
+    public struct Abonement 
+    {
+        Guid Id { get; set; }
+        DateTime EndDate { get; set; }
+        Human Human { get; set; }
+    }
+    public abstract class Human 
+    {
+        public string Name { get; private set; }
+    }
+
+
+    public class Admin 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public interface IGym 
+    {
+
+    }
+
+    public class Gym
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public Admin CurrentAdmin { get; set; }
+
+        public List<Section> Sections = new List<Section>();
+        public List<Abonement> Abonements { get; set; }
+    }
+
+    public interface IfitnessClub 
+    {
+        // bool Act { get; }// не отвечает не за какое поведение - это не правильно - все залы можно назвать фитнесзалом
+        List<Schedule> GetFitnessSchedule();
+    }
+
+    public abstract class Schedule 
+    {
+        public Section Section { get; set; }
+        public Human Trainer { get; set; }
+    }
+
+    public class Section 
+    {
+        public string Name { get; set; }
+        public SectionType Type { get; set; }
+    }
+
+    public enum SectionType 
+    {
+        Fitness
+    }
+}
---
+++ Practice13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice13
+{
+    /*
+    public interface IBasketballPlayer
+    {
+        int Throw();
+    }
+
+    public abstract class Human 
+    {
+        public string Name { get; set; }
+    }
+
+    public abstract class BasketBallMen : Human, IBasketballPlayer
+    {
+        public abstract int Throw();
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+    public class Forward : BasketBallMen
+    {
+        public override int Throw()
+        {
+            return 3;//throw new NotImplementedException();
+        }
+    }
+
+    public class Guard : BasketBallMen
+    {
+        public override int Throw()
+        {
+            return -1;//throw new NotImplementedException();
+        }
+    }
+    */
+    class Program
+    {
+        /*public static List<IBasketballPlayer> Team { get; set; }*/
+        static void Main(string[] args)
+        {
+            /*
+            var team = new List<IBasketballPlayer>();
+            team.Add(new Guard{Name = "Igor" });
+            team[0].Throw();
+
+            var type = team[0].GetType();
+            */
+
+        }
+    }
+}
